17eb1b757210372884bfa3dc7f196619804ba850
Erik Olson <oerik100@gmail.com>
2023-01-05 14:03:14 -0500

    adding capability to add values of cards into and input and display on screen

--- js/main.js
+++ js/main.js
@@ -29,7 +29,10 @@ function getFetch(){
         document.querySelector('#player1_2').src = data.cards[1].image
         document.querySelector('#dealer1').src = data.cards[2].image
         document.querySelector('#dealer1_2').src = data.cards[3].image
-        
+        document.querySelector('#showMe').value = Number(data.cards[0].value) + Number(data.cards[1].value)
+
+
+
         // if(val1 + val2 < 21) {
         //   document.querySelector('#score').innerText = val1 + val2
         // }else {
@@ -47,9 +50,10 @@ function getFetch(){
           
           document.querySelector('#hitMeCardOne').src = data.cards[curNum].image
           document.querySelector('#hitMeTwo').classList.toggle('hidden')
+          document.querySelector('#showMe').value = Number(data.cards[0].value) + Number(data.cards[1].value) + Number(data.cards[4].value)
+      
           document.querySelector('#hitMe').classList.toggle('hidden')
           return curNum = curNum + 1
-          
         }
         console.log(curNum)
 
@@ -87,44 +91,27 @@ function getFetch(){
   };
 
 
-    // function hitMeOnce() {
-    //   const urlNew = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-
-    //   fetch(urlNew)
-    //     .then(res => res.json())
-    //     .then(data1 => {
-    //       console.log(data1)
-    //     })
-    // }
 
 
-  //   function hitMeOnce() {
-  //     const urlNew = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
 
 
-  // fetch(urlNew)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       let val3 = Number(cardValue(data.cards[3].value))
-  //       // let hitMeCardOne = Number(cardValue(data.cards[0].value))
-  //       document.querySelector('#hitMeCardOne').src = data.cards[0].image
-  //       if(val1 + val2 + val3 < 21) {
-  //         document.querySelector('#score').innerText = val1 + val2 + val3
-  //       }else {
-  //         document.querySelector('#score').innerText = 'BUST'
-  //       }
-  //     })
-  //   };
 
-  //   function stayRightThere() {
-  //     const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`
-      
-  //     fetch(url)
-  //     .then(res => res.json()) // parse response as JSON
-  //     .then(data => {
-  //     document.querySelector('#dealer1_2').src = data.cards[3].image
-  //     })
-  //   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     // function cardValue(val){
